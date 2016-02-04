547648f569a69eca51be93d815f05b31f5d767a7
KwonKyungsoon <bh3kks@naver.com>
2016-02-04 17:48:44 +0900

    chat_view 구성 중 refresh 처리 문제 발생

--- Challedge/urls.py
+++ Challedge/urls.py
@@ -5,8 +5,6 @@ urlpatterns = patterns('',
     
 	url(r'^main/$', 'chatting.views.main_view', name='main_view'),
 
-	url(r'^login_check/$', 'chatting.views.login_check', name='login_check'),
-
 	url(r'^chat/$', 'chatting.views.chat_view', name='chat_view'),
 
     url(r'^admin/', include(admin.site.urls)),
--- chatting/views.py
+++ chatting/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, render_to_response, RequestContext
 from django.http import HttpResponse
-from .models import User
+from .models import User, ChattingRoom
 import templates 
 
 # Create your views here.
@@ -8,30 +8,36 @@ def main_view(request):
 	# 메인 화면 출력
 	return render(request, 'main_view.html')
 
-def login_check(request):
+def chat_view(request):
 	# 로그인이 유효한지 체크
 
+	chattingRoom = ChattingRoom.objects.all()
 	username = ''
-	if 'username' in request.POST:
-		if len(request.POST['username']) == 0:
-			# 아무 글자도 입력 안되었을 때
-			return HttpResponse('글자를 입력하세요.')
-		else:
-			username = request.POST['username']
-	else: 
-		return HttpResponse('False in Post')
 
-	try:
-		if User.objects.get(username=username):
-			return HttpResponse('ID 중복')
-	except:
-		user = User(username=username)
-		user.save()
-		return redirect('/chat/%s/' % username)
+	if request.method == "GET":
+		user = request.user
+		username = user.username
 
-def chat_view(request):
-	# 채팅 화면 출력
-
-
-
-	return render(request, 'chat_view.html')
+	elif request.method == "POST":
+		if 'username' in request.POST:
+			if len(request.POST['username']) == 0:
+				# 아무 글자도 입력 안되었을 때
+				return HttpResponse('글자를 입력하세요.')
+			else:
+				username = request.POST['username']
+		else: 
+			return HttpResponse('False in Post')
+		try:
+			if User.objects.get(username=username):
+				return HttpResponse('ID 중복')
+		except:
+			user = User(username=username)
+			user.save()
+	
+	return render(request, "chat_view.html", 
+		{
+			"username": username,
+			"chattingRoom": chattingRoom,
+		}
+	)
+	
