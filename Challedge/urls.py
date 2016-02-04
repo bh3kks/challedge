1be9e17487307c6d616975226f4d60a075fce197
KwonKyungsoon <bh3kks@naver.com>
2016-02-04 15:43:44 +0900

    url / model

--- Challedge/urls.py
+++ Challedge/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Challedge.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    
+	url(r'^main/$', 'chatting.views.main_view', name='main_view'),
+	# 메인 화면 url
+
+	url(r'^chat/$', 'chatting.views.chat_view', name='chat_view'),
+	# 메인 화면 url
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- chatting/models.py
+++ chatting/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
-class Test():
-	pass
+class User(models.Model):
+	username = models.TextField(max_length=20, blank=False)
+	created = models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+	# 채팅 한 마디
+	user = models.ForeignKey(User)
+	content = models.TextField(max_length=200, blank=False)
+	created = models.DateTimeField(auto_now_add=True)
+
+class ChattingRoom(models.Model):
+	# 대학교별 채팅 방
+	name = models.TextField(max_length=50, blank=False)
+	max_user = models.IntegerField(null=30)
+	cur_user = models.IntegerField(null=0)
