485421e0e08f6292cefd784216f0d339e3ade004
KwonKyungsoon <bh3kks@naver.com>
2016-02-04 16:37:29 +0900

    login check 기능 추가

--- Challedge/urls.py
+++ Challedge/urls.py
@@ -4,11 +4,10 @@ from django.contrib import admin
 urlpatterns = patterns('',
     
 	url(r'^main/$', 'chatting.views.main_view', name='main_view'),
-	# 메인 화면 url
+
+	url(r'^login_check/$', 'chatting.views.login_check', name='login_check'),
 
 	url(r'^chat/$', 'chatting.views.chat_view', name='chat_view'),
-	# 메인 화면 url
-
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- chatting/admin.py
+++ chatting/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from chatting.models import User, Comments, ChattingRoom
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Comments)
+admin.site.register(ChattingRoom)
